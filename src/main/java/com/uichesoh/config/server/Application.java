a2fdc4e0e553b28596538154949c93043b255715
Samuel Purriños <samuel.purrinos@outlook.com>
2023-04-11 17:11:15 +0200

    Create project

---
+++ src/main/java/com/uichesoh/config/server/Application.java
@@ -0,0 +1,13 @@
+package com.uichesoh.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
